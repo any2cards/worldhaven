d4d9140f1bbfdf5d93281601687b0887f8c69df7
any2cards <any2cards@yahoo.com>
2021-11-05 16:43:58 -0700

    Implement FH MS

--- data/monster-stat-cards.js
+++ data/monster-stat-cards.js
@@ -824,5 +824,159 @@
     "expansion": "Forgotten Circles",
     "image": "monster-stat-cards/fc-valrath-tracker-4.png",
     "xws": "valrathtracker"
+  },
+  {
+    "name": "abael scout",
+    "points": 118,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-abael-scout-0.png",
+    "xws": "abaelscout"
+  },
+  {
+    "name": "algox archer",
+    "points": 119,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-algox-archer-0.png",
+    "xws": "algoxarcher"
+  },
+  {
+    "name": "algox guard",
+    "points": 120,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-algox-guard-0.png",
+    "xws": "algoxguard"
+  },
+  {
+    "name": "algox scout",
+    "points": 121,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-algox-scout-0.png",
+    "xws": "algoxscout"
+  },
+  {
+    "name": "algox shaman",
+    "points": 122,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-algox-shaman-0.png",
+    "xws": "algoxshaman"
+  },
+  {
+    "name": "burrowing blade",
+    "points": 123,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-burrowing-blade-0.png",
+    "xws": "burrowingblade"
+  },
+  {
+    "name": "chaos demon",
+    "points": 124,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-chaos-demon-0.png",
+    "xws": "chaosdemon"
+  },
+  {
+    "name": "flaming bladespinner",
+    "points": 125,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-flaming-bladespinner-0.png",
+    "xws": "flamingbladespinner"
+  },
+  {
+    "name": "frozen corpse",
+    "points": 126,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-frozen-corpse-0.png",
+    "xws": "frozencorpse"
+  },
+  {
+    "name": "ice wraith",
+    "points": 127,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-ice-wraith-0.png",
+    "xws": "icewraith"
+  },
+  {
+    "name": "lightning eel",
+    "points": 128,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-lightning-eel-0.png",
+    "xws": "lightningeel"
+  },
+  {
+    "name": "living doom",
+    "points": 129,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-living-doom-0.png",
+    "xws": "livingdoom"
+  },
+  {
+    "name": "lurker clawcrusher",
+    "points": 130,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-lurker-clawcrusher-0.png",
+    "xws": "lurkerclawcrusher"
+  },
+  {
+    "name": "lurker mindsnipper",
+    "points": 131,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-lurker-mindsnipper-0.png",
+    "xws": "lurkermindsnipper"
+  },
+  {
+    "name": "lurker wavethrower",
+    "points": 132,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-lurker-wavethrower-0.png",
+    "xws": "lurkerwavethrower"
+  },
+  {
+    "name": "piranha pig",
+    "points": 133,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-piranha-pig-0.png",
+    "xws": "piranhapig"
+  },
+  {
+    "name": "polar bear",
+    "points": 134,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-polar-bear-0.png",
+    "xws": "polarbear"
+  },
+  {
+    "name": "robotic boltshooter",
+    "points": 135,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-robotic-boltshooter-0.png",
+    "xws": "roboticboltshooter"
+  },
+  {
+    "name": "ruined machine",
+    "points": 136,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-ruined-machine-0.png",
+    "xws": "ruinedmachine"
+  },
+  {
+    "name": "shrike fiend",
+    "points": 137,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-shrike-fiend-0.png",
+    "xws": "shrikefiend"
+  },
+  {
+    "name": "snow imp",
+    "points": 138,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-snow-imp-0.png",
+    "xws": "snowimp"
+  },
+  {
+    "name": "steel automaton",
+    "points": 139,
+    "expansion": "Frosthaven",
+    "image": "monster-stat-cards/fh-steel-automaton-0.png",
+    "xws": "steelautomaton"
   }
 ]
