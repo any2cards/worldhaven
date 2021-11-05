bc4e7d0704c40c736f436c6709237e0d7c8d5571
any2cards <any2cards@yahoo.com>
2021-11-04 18:46:50 -0700

    Implement ENC for CAR

--- data/character-ability-cards-revised.js
+++ data/character-ability-cards-revised.js
@@ -3,315 +3,315 @@
     "name": "bt-back",
     "points": 0,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/BT/bt-back.png",
+    "image": "character-ability-cards-revised/BT/gh-bt-back.png",
     "xws": "btback"
   },
   {
     "name": "disorienting roar",
     "points": 1,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/BT/disorienting-roar.png",
+    "image": "character-ability-cards-revised/BT/gh-disorienting-roar.png",
     "xws": "disorientingroar"
   },
   {
     "name": "primal blessing",
     "points": 2,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/BT/primal-blessing.png",
+    "image": "character-ability-cards-revised/BT/gh-primal-blessing.png",
     "xws": "primalblessing"
   },
   {
     "name": "spirit swap",
     "points": 3,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/BT/spirit-swap.png",
+    "image": "character-ability-cards-revised/BT/gh-spirit-swap.png",
     "xws": "spiritswap"
   },
   {
     "name": "anguish and salvation",
     "points": 4,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/anguish-and-salvation.png",
+    "image": "character-ability-cards-revised/DR/fc-anguish-and-salvation.png",
     "xws": "anguishandsalvation"
   },
   {
     "name": "anticipate intricacies",
     "points": 5,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/anticipate-intricacies.png",
+    "image": "character-ability-cards-revised/DR/fc-anticipate-intricacies.png",
     "xws": "anticipateintricacies"
   },
   {
     "name": "bad omen",
     "points": 6,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/bad-omen.png",
+    "image": "character-ability-cards-revised/DR/fc-bad-omen.png",
     "xws": "badomen"
   },
   {
     "name": "call of the nether",
     "points": 7,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/call-of-the-nether.png",
+    "image": "character-ability-cards-revised/DR/fc-call-of-the-nether.png",
     "xws": "callofthenether"
   },
   {
     "name": "careful attunement",
     "points": 8,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/careful-attunement.png",
+    "image": "character-ability-cards-revised/DR/fc-careful-attunement.png",
     "xws": "carefulattunement"
   },
   {
     "name": "clairvoyance",
     "points": 9,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/clairvoyance.png",
+    "image": "character-ability-cards-revised/DR/fc-clairvoyance.png",
     "xws": "clairvoyance"
   },
   {
     "name": "cleansing rite",
     "points": 10,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/cleansing-rite.png",
+    "image": "character-ability-cards-revised/DR/fc-cleansing-rite.png",
     "xws": "cleansingrite"
   },
   {
     "name": "curative flux",
     "points": 11,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/curative-flux.png",
+    "image": "character-ability-cards-revised/DR/fc-curative-flux.png",
     "xws": "curativeflux"
   },
   {
     "name": "cursed ground",
     "points": 12,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/cursed-ground.png",
+    "image": "character-ability-cards-revised/DR/fc-cursed-ground.png",
     "xws": "cursedground"
   },
   {
     "name": "deep contemplation",
     "points": 13,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/deep-contemplation.png",
+    "image": "character-ability-cards-revised/DR/fc-deep-contemplation.png",
     "xws": "deepcontemplation"
   },
   {
     "name": "dimensional divide",
     "points": 14,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/dimensional-divide.png",
+    "image": "character-ability-cards-revised/DR/fc-dimensional-divide.png",
     "xws": "dimensionaldivide"
   },
   {
     "name": "dimensional transfer",
     "points": 15,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/dimensional-transfer.png",
+    "image": "character-ability-cards-revised/DR/fc-dimensional-transfer.png",
     "xws": "dimensionaltransfer"
   },
   {
     "name": "dr-back",
     "points": 16,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/dr-back.png",
+    "image": "character-ability-cards-revised/DR/fc-dr-back.png",
     "xws": "drback"
   },
   {
     "name": "duality shards",
     "points": 17,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/duality-shards.png",
+    "image": "character-ability-cards-revised/DR/fc-duality-shards.png",
     "xws": "dualityshards"
   },
   {
     "name": "enfeebling hex",
     "points": 18,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/enfeebling-hex.png",
+    "image": "character-ability-cards-revised/DR/fc-enfeebling-hex.png",
     "xws": "enfeeblinghex"
   },
   {
     "name": "envision the course",
     "points": 19,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/envision-the-course.png",
+    "image": "character-ability-cards-revised/DR/fc-envision-the-course.png",
     "xws": "envisionthecourse"
   },
   {
     "name": "ethereal vortex",
     "points": 20,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/ethereal-vortex.png",
+    "image": "character-ability-cards-revised/DR/fc-ethereal-vortex.png",
     "xws": "etherealvortex"
   },
   {
     "name": "gift of the void",
     "points": 21,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/gift-of-the-void.png",
+    "image": "character-ability-cards-revised/DR/fc-gift-of-the-void.png",
     "xws": "giftofthevoid"
   },
   {
     "name": "hand of destiny",
     "points": 22,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/hand-of-destiny.png",
+    "image": "character-ability-cards-revised/DR/fc-hand-of-destiny.png",
     "xws": "handofdestiny"
   },
   {
     "name": "inspiration from beyond",
     "points": 23,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/inspiration-from-beyond.png",
+    "image": "character-ability-cards-revised/DR/fc-inspiration-from-beyond.png",
     "xws": "inspirationfrombeyond"
   },
   {
     "name": "otherworldly journey",
     "points": 24,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/otherworldly-journey.png",
+    "image": "character-ability-cards-revised/DR/fc-otherworldly-journey.png",
     "xws": "otherworldlyjourney"
   },
   {
     "name": "peer into battle",
     "points": 25,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/peer-into-battle.png",
+    "image": "character-ability-cards-revised/DR/fc-peer-into-battle.png",
     "xws": "peerintobattle"
   },
   {
     "name": "planar fissure",
     "points": 26,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/planar-fissure.png",
+    "image": "character-ability-cards-revised/DR/fc-planar-fissure.png",
     "xws": "planarfissure"
   },
   {
     "name": "preordain the path",
     "points": 27,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/preordain-the-path.png",
+    "image": "character-ability-cards-revised/DR/fc-preordain-the-path.png",
     "xws": "preordainthepath"
   },
   {
     "name": "protective aura",
     "points": 28,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/protective-aura.png",
+    "image": "character-ability-cards-revised/DR/fc-protective-aura.png",
     "xws": "protectiveaura"
   },
   {
     "name": "ray of light",
     "points": 29,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/ray-of-light.png",
+    "image": "character-ability-cards-revised/DR/fc-ray-of-light.png",
     "xws": "rayoflight"
   },
   {
     "name": "revitalizing fount",
     "points": 30,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/revitalizing-fount.png",
+    "image": "character-ability-cards-revised/DR/fc-revitalizing-fount.png",
     "xws": "revitalizingfount"
   },
   {
     "name": "seal their fate",
     "points": 31,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/seal-their-fate.png",
+    "image": "character-ability-cards-revised/DR/fc-seal-their-fate.png",
     "xws": "sealtheirfate"
   },
   {
     "name": "void snare",
     "points": 32,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/void-snare.png",
+    "image": "character-ability-cards-revised/DR/fc-void-snare.png",
     "xws": "voidsnare"
   },
   {
     "name": "ghost falcons",
     "points": 33,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/envision-the-course.png",
+    "image": "character-ability-cards-revised/DR/fc-envision-the-course.png",
     "xws": "envisionthecourse"
   },
   {
     "name": "twilight archon",
     "points": 34,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/DR/planar-fissure.png",
+    "image": "character-ability-cards-revised/DR/fc-planar-fissure.png",
     "xws": "planarfissure"
   },
   {
     "name": "ds-back",
     "points": 35,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/DS/ds-back.png",
+    "image": "character-ability-cards-revised/DS/gh-ds-back.png",
     "xws": "dsback"
   },
   {
     "name": "felling swoop",
     "points": 36,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/DS/felling-swoop.png",
+    "image": "character-ability-cards-revised/DS/gh-felling-swoop.png",
     "xws": "fellingswoop"
   },
   {
     "name": "concealed dominance",
     "points": 37,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/NS/concealed-dominance.png",
+    "image": "character-ability-cards-revised/NS/gh-concealed-dominance.png",
     "xws": "concealeddominance"
   },
   {
     "name": "ns-back",
     "points": 38,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/NS/ns-back.png",
+    "image": "character-ability-cards-revised/NS/gh-ns-back.png",
     "xws": "nsback"
   },
   {
     "name": "otherworldly rage",
     "points": 39,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/SU/otherworldly-rage.png",
+    "image": "character-ability-cards-revised/SU/gh-otherworldly-rage.png",
     "xws": "otherworldlyrage"
   },
   {
     "name": "su-back",
     "points": 40,
     "expansion": "Gloomhaven",
-    "image": "character-ability-cards-revised/SU/su-back.png",
+    "image": "character-ability-cards-revised/SU/gh-su-back.png",
     "xws": "suback"
   },
   {
     "name": "halt-bottom-back",
     "points": 41,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/ZZ/halt-bottom-back.png",
+    "image": "character-ability-cards-revised/ZZ/fc-halt-bottom-back.png",
     "xws": "haltbottomback"
   },
   {
     "name": "halt-bottom-front",
     "points": 42,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/ZZ/halt-bottom-front.png",
+    "image": "character-ability-cards-revised/ZZ/fc-halt-bottom-front.png",
     "xws": "haltbottomfront"
   },
   {
     "name": "halt-top-back",
     "points": 43,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/ZZ/halt-top-back.png",
+    "image": "character-ability-cards-revised/ZZ/fc-halt-top-back.png",
     "xws": "halttopback"
   },
   {
     "name": "halt-top-front",
     "points": 44,
     "expansion": "Forgotten Circles",
-    "image": "character-ability-cards-revised/ZZ/halt-top-front.png",
+    "image": "character-ability-cards-revised/ZZ/fc-halt-top-front.png",
     "xws": "halttopfront"
   }
 ]
