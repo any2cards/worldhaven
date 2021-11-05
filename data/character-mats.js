0f6937231bc7ae88dd9c48dcac4acf369f0a4bdc
any2cards <any2cards@yahoo.com>
2021-11-05 15:01:55 -0700

    Implement FH CM

--- data/character-mats.js
+++ data/character-mats.js
@@ -418,5 +418,89 @@
     "expansion": "Forgotten Circles",
     "image": "character-mats/fc-diviner-back.png",
     "xws": "diviner"
+  },
+  {
+    "name": "banner spear",
+    "points": 60,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-banner-spear.png",
+    "xws": "bannerspear"
+  },
+  {
+    "name": "banner spear",
+    "points": 61,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-banner-spear-back.png",
+    "xws": "bannerspear"
+  },
+  {
+    "name": "blinkblade",
+    "points": 62,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-blinkblade.png",
+    "xws": "blinkblade"
+  },
+  {
+    "name": "blinkblade",
+    "points": 63,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-blinkblade-back.png",
+    "xws": "blinkblade"
+  },
+  {
+    "name": "boneshaper",
+    "points": 64,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-boneshaper.png",
+    "xws": "boneshaper"
+  },
+  {
+    "name": "boneshaper",
+    "points": 65,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-boneshaper-back.png",
+    "xws": "boneshaper"
+  },
+  {
+    "name": "deathwalker",
+    "points": 66,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-deathwalker.png",
+    "xws": "deathwalker"
+  },
+  {
+    "name": "deathwalker",
+    "points": 67,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-deathwalker-back.png",
+    "xws": "deathwalker"
+  },
+  {
+    "name": "drifter",
+    "points": 68,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-drifter.png",
+    "xws": "drifter"
+  },
+  {
+    "name": "drifter",
+    "points": 69,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-drifter-back.png",
+    "xws": "drifter"
+  },
+  {
+    "name": "geminate",
+    "points": 70,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-geminate.png",
+    "xws": "geminate"
+  },
+  {
+    "name": "geminate",
+    "points": 71,
+    "expansion": "Frosthaven",
+    "image": "character-mats/fh-geminate-back.png",
+    "xws": "geminate"
   }
 ]
