34f4bf67c64b4027303ab7a6a5e7213f2fb764a0
any2cards <any2cards@yahoo.com>
2021-11-08 13:38:25 -0800

    Implement SDS for PAC

--- data/player-aid-cards.js
+++ data/player-aid-cards.js
@@ -3,56 +3,56 @@
     "name": "player reference front",
     "points": 0,
     "expansion": "Gloomhaven",
-    "image": "player-aid-cards/gh-player-reference-front.png",
+    "image": "player-aid-cards/gloomhaven/gh-player-reference-front.png",
     "xws": "playerreferencefront"
   },
   {
     "name": "player reference back",
     "points": 1,
     "expansion": "Gloomhaven",
-    "image": "player-aid-cards/gh-player-reference-back.png",
+    "image": "player-aid-cards/gloomhaven/gh-player-reference-back.png",
     "xws": "playerreferenceback"
   },
   {
     "name": "halt bottom front",
     "points": 2,
     "expansion": "Forgotten Circles",
-    "image": "player-aid-cards/fc-halt-bottom-front.png",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-bottom-front.png",
     "xws": "haltbottomfront"
   },
   {
     "name": "halt bottom back",
     "points": 3,
     "expansion": "Forgotten Circles",
-    "image": "player-aid-cards/fc-halt-bottom-back.png",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-bottom-back.png",
     "xws": "haltbottomback"
   },
   {
     "name": "halt top front",
     "points": 4,
     "expansion": "Forgotten Circles",
-    "image": "player-aid-cards/fc-halt-top-front.png",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-top-front.png",
     "xws": "halttopfront"
   },
   {
     "name": "halt top back",
     "points": 5,
     "expansion": "Forgotten Circles",
-    "image": "player-aid-cards/fc-halt-top-back.png",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-top-back.png",
     "xws": "halttopback"
   },
   {
     "name": "player reference front",
     "points": 6,
     "expansion": "Forgotten Circles",
-    "image": "player-aid-cards/fc-player-reference-front.png",
+    "image": "player-aid-cards/forgotten-circles/fc-player-reference-front.png",
     "xws": "playerreferencefront"
   },
   {
     "name": "player reference back",
     "points": 7,
     "expansion": "Forgotten Circles",
-    "image": "player-aid-cards/fc-player-reference-back.png",
+    "image": "player-aid-cards/forgotten-circles/fc-player-reference-back.png",
     "xws": "playerreferenceback"
   }
 ]
