dc732cddfef28ba02877fcd91fd472582c962e56
any2cards <any2cards@yahoo.com>
2021-11-08 09:39:25 -0800

    Implement SDS for AM, BG, CA, CAR

--- data/battle-goals.js
+++ data/battle-goals.js
@@ -3,406 +3,406 @@
     "name": "battle-goals-back",
     "points": 0,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-battle-goals-back.png",
+    "image": "battle-goals/gloomhaven/gh-battle-goals-back.png",
     "xws": "battlegoalsback"
   },
   {
     "name": "aggressor",
     "points": 1,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-aggressor.png",
+    "image": "battle-goals/gloomhaven/gh-aggressor.png",
     "xws": "aggressor"
   },
   {
     "name": "diehard",
     "points": 2,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-diehard.png",
+    "image": "battle-goals/gloomhaven/gh-diehard.png",
     "xws": "diehard"
   },
   {
     "name": "dynamo",
     "points": 3,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-dynamo.png",
+    "image": "battle-goals/gloomhaven/gh-dynamo.png",
     "xws": "dynamo"
   },
   {
     "name": "executioner",
     "points": 4,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-executioner.png",
+    "image": "battle-goals/gloomhaven/gh-executioner.png",
     "xws": "executioner"
   },
   {
     "name": "explorer",
     "points": 5,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-explorer.png",
+    "image": "battle-goals/gloomhaven/gh-explorer.png",
     "xws": "explorer"
   },
   {
     "name": "fast healer",
     "points": 6,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-fast-healer.png",
+    "image": "battle-goals/gloomhaven/gh-fast-healer.png",
     "xws": "fasthealer"
   },
   {
     "name": "hoarder",
     "points": 7,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-hoarder.png",
+    "image": "battle-goals/gloomhaven/gh-hoarder.png",
     "xws": "hoarder"
   },
   {
     "name": "hunter",
     "points": 8,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-hunter.png",
+    "image": "battle-goals/gloomhaven/gh-hunter.png",
     "xws": "hunter"
   },
   {
     "name": "indigent",
     "points": 9,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-indigent.png",
+    "image": "battle-goals/gloomhaven/gh-indigent.png",
     "xws": "indigent"
   },
   {
     "name": "layabout",
     "points": 10,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-layabout.png",
+    "image": "battle-goals/gloomhaven/gh-layabout.png",
     "xws": "layabout"
   },
   {
     "name": "masochist",
     "points": 11,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-masochist.png",
+    "image": "battle-goals/gloomhaven/gh-masochist.png",
     "xws": "masochist"
   },
   {
     "name": "neutralizer",
     "points": 12,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-neutralizer.png",
+    "image": "battle-goals/gloomhaven/gh-neutralizer.png",
     "xws": "neutralizer"
   },
   {
     "name": "opener",
     "points": 13,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-opener.png",
+    "image": "battle-goals/gloomhaven/gh-opener.png",
     "xws": "opener"
   },
   {
     "name": "pacifist",
     "points": 14,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-pacifist.png",
+    "image": "battle-goals/gloomhaven/gh-pacifist.png",
     "xws": "pacifist"
   },
   {
     "name": "plunderer",
     "points": 15,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-plunderer.png",
+    "image": "battle-goals/gloomhaven/gh-plunderer.png",
     "xws": "plunderer"
   },
   {
     "name": "professional",
     "points": 16,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-professional.png",
+    "image": "battle-goals/gloomhaven/gh-professional.png",
     "xws": "professional"
   },
   {
     "name": "protector",
     "points": 17,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-protector.png",
+    "image": "battle-goals/gloomhaven/gh-protector.png",
     "xws": "protector"
   },
   {
     "name": "purist",
     "points": 18,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-purist.png",
+    "image": "battle-goals/gloomhaven/gh-purist.png",
     "xws": "purist"
   },
   {
     "name": "sadist",
     "points": 19,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-sadist.png",
+    "image": "battle-goals/gloomhaven/gh-sadist.png",
     "xws": "sadist"
   },
   {
     "name": "scrambler",
     "points": 20,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-scrambler.png",
+    "image": "battle-goals/gloomhaven/gh-scrambler.png",
     "xws": "scrambler"
   },
   {
     "name": "straggler",
     "points": 21,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-straggler.png",
+    "image": "battle-goals/gloomhaven/gh-straggler.png",
     "xws": "straggler"
   },
   {
     "name": "streamliner",
     "points": 22,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-streamliner.png",
+    "image": "battle-goals/gloomhaven/gh-streamliner.png",
     "xws": "streamliner"
   },
   {
     "name": "workhorse",
     "points": 23,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-workhorse.png",
+    "image": "battle-goals/gloomhaven/gh-workhorse.png",
     "xws": "workhorse"
   },
   {
     "name": "zealot",
     "points": 24,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/battle-goals/gh-zealot.png",
+    "image": "battle-goals/gloomhaven/gh-zealot.png",
     "xws": "zealot"
   },
   {
     "name": "battle-goals-back",
     "points": 25,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-battle-goals-back.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-battle-goals-back.png",
     "xws": "battlegoalsback"
   },
   {
     "name": "acrobat",
     "points": 26,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-acrobat.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-acrobat.png",
     "xws": "acrobat"
   },
   {
     "name": "agoraphobe",
     "points": 27,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-agoraphobe.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-agoraphobe.png",
     "xws": "agoraphobe"
   },
   {
     "name": "altruist",
     "points": 28,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-altruist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-altruist.png",
     "xws": "altruist"
   },
   {
     "name": "assistant",
     "points": 29,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-assistant.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-assistant.png",
     "xws": "assistant"
   },
   {
     "name": "challenger",
     "points": 30,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-challenger.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-challenger.png",
     "xws": "challenger"
   },
   {
     "name": "closer",
     "points": 31,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-closer.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-closer.png",
     "xws": "closer"
   },
   {
     "name": "conservator",
     "points": 32,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-conservator.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-conservator.png",
     "xws": "conservator"
   },
   {
     "name": "diehard",
     "points": 33,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-diehard.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-diehard.png",
     "xws": "diehard"
   },
   {
     "name": "egoist",
     "points": 34,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-egoist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-egoist.png",
     "xws": "egoist"
   },
   {
     "name": "fast healer",
     "points": 35,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-fast-healer.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-fast-healer.png",
     "xws": "fasthealer"
   },
   {
     "name": "gambler",
     "points": 36,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-gambler.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-gambler.png",
     "xws": "gambler"
   },
   {
     "name": "hothead",
     "points": 37,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-hothead.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-hothead.png",
     "xws": "hothead"
   },
   {
     "name": "insomniac",
     "points": 38,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-insomniac.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-insomniac.png",
     "xws": "insomniac"
   },
   {
     "name": "masochist",
     "points": 39,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-masochist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-masochist.png",
     "xws": "masochist"
   },
   {
     "name": "mugger",
     "points": 40,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-mugger.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-mugger.png",
     "xws": "mugger"
   },
   {
     "name": "opener",
     "points": 41,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-opener.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-opener.png",
     "xws": "opener"
   },
   {
     "name": "pacifist",
     "points": 42,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-pacifist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-pacifist.png",
     "xws": "pacifist"
   },
   {
     "name": "pickpocket",
     "points": 43,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-pickpocket.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-pickpocket.png",
     "xws": "pickpocket"
   },
   {
     "name": "pincushion",
     "points": 44,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-pincushion.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-pincushion.png",
     "xws": "pincushion"
   },
   {
     "name": "plebeian",
     "points": 45,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-plebeian.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-plebeian.png",
     "xws": "plebeian"
   },
   {
     "name": "prohibitionist",
     "points": 46,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-prohibitionist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-prohibitionist.png",
     "xws": "prohibitionist"
   },
   {
     "name": "ravager",
     "points": 47,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-ravager.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-ravager.png",
     "xws": "ravager"
   },
   {
     "name": "recluse",
     "points": 48,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-recluse.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-recluse.png",
     "xws": "recluse"
   },
   {
     "name": "sadist",
     "points": 49,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-sadist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-sadist.png",
     "xws": "sadist"
   },
   {
     "name": "scrambler",
     "points": 50,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-scrambler.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-scrambler.png",
     "xws": "scrambler"
   },
   {
     "name": "shadow",
     "points": 51,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-shadow.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-shadow.png",
     "xws": "shadow"
   },
   {
     "name": "shirker",
     "points": 52,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-shirker.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-shirker.png",
     "xws": "shirker"
   },
   {
     "name": "specialist",
     "points": 53,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-specialist.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-specialist.png",
     "xws": "specialist"
   },
   {
     "name": "straggler",
     "points": 54,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-straggler.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-straggler.png",
     "xws": "straggler"
   },
   {
     "name": "trailblazer",
     "points": 55,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-trailblazer.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-trailblazer.png",
     "xws": "trailblazer"
   },
   {
     "name": "transmitter",
     "points": 56,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-transmitter.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-transmitter.png",
     "xws": "transmitter"
   },
   {
     "name": "weakling",
     "points": 57,
     "expansion": "Jaws Of The Lion",
-    "image": "jaws-of-the-lion/battle-goals/jl-weakling.png",
+    "image": "battle-goals/jaws-of-the-lion/jl-weakling.png",
     "xws": "weakling"
   }
 ]
--- data/character-ability-cards-revised.js
+++ data/character-ability-cards-revised.js
@@ -3,287 +3,287 @@
     "name": "bt-back",
     "points": 0,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/BT/gh-bt-back.png",
+    "image": "character-ability-cards-revised/gloomhaven/BT/gh-bt-back.png",
     "xws": "btback"
   },
   {
     "name": "disorienting roar",
     "points": 1,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/BT/gh-disorienting-roar.png",
+    "image": "character-ability-cards-revised/gloomhaven/BT/gh-disorienting-roar.png",
     "xws": "disorientingroar"
   },
   {
     "name": "primal blessing",
     "points": 2,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/BT/gh-primal-blessing.png",
+    "image": "character-ability-cards-revised/gloomhaven/BT/gh-primal-blessing.png",
     "xws": "primalblessing"
   },
   {
     "name": "spirit swap",
     "points": 3,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/BT/gh-spirit-swap.png",
+    "image": "character-ability-cards-revised/gloomhaven/BT/gh-spirit-swap.png",
     "xws": "spiritswap"
   },
   {
     "name": "ds-back",
     "points": 4,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/DS/gh-ds-back.png",
+    "image": "character-ability-cards-revised/gloomhaven/DS/gh-ds-back.png",
     "xws": "dsback"
   },
   {
     "name": "felling swoop",
     "points": 5,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/DS/gh-felling-swoop.png",
+    "image": "character-ability-cards-revised/gloomhaven/DS/gh-felling-swoop.png",
     "xws": "fellingswoop"
   },
   {
     "name": "ns-back",
     "points": 6,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/NS/gh-ns-back.png",
+    "image": "character-ability-cards-revised/gloomhaven/NS/gh-ns-back.png",
     "xws": "nsback"
   },
   {
     "name": "concealed dominance",
     "points": 7,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/NS/gh-concealed-dominance.png",
+    "image": "character-ability-cards-revised/gloomhaven/NS/gh-concealed-dominance.png",
     "xws": "concealeddominance"
   },
   {
     "name": "su-back",
     "points": 8,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/SU/gh-su-back.png",
+    "image": "character-ability-cards-revised/gloomhaven/SU/gh-su-back.png",
     "xws": "suback"
   },
   {
     "name": "otherworldly rage",
     "points": 9,
     "expansion": "Gloomhaven",
-    "image": "gloomhaven/character-ability-cards-revised/SU/gh-otherworldly-rage.png",
+    "image": "character-ability-cards-revised/gloomhaven/SU/gh-otherworldly-rage.png",
     "xws": "otherworldlyrage"
   },
   {
     "name": "dr-back",
     "points": 10,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-dr-back.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-dr-back.png",
     "xws": "drback"
   },
   {
     "name": "anguish and salvation",
     "points": 11,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-anguish-and-salvation.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-anguish-and-salvation.png",
     "xws": "anguishandsalvation"
   },
   {
     "name": "anticipate intricacies",
     "points": 12,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-anticipate-intricacies.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-anticipate-intricacies.png",
     "xws": "anticipateintricacies"
   },
   {
     "name": "bad omen",
     "points": 13,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-bad-omen.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-bad-omen.png",
     "xws": "badomen"
   },
   {
     "name": "call of the nether",
     "points": 14,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-call-of-the-nether.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-call-of-the-nether.png",
     "xws": "callofthenether"
   },
   {
     "name": "careful attunement",
     "points": 15,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-careful-attunement.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-careful-attunement.png",
     "xws": "carefulattunement"
   },
   {
     "name": "clairvoyance",
     "points": 16,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-clairvoyance.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-clairvoyance.png",
     "xws": "clairvoyance"
   },
   {
     "name": "cleansing rite",
     "points": 17,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-cleansing-rite.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-cleansing-rite.png",
     "xws": "cleansingrite"
   },
   {
     "name": "curative flux",
     "points": 18,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-curative-flux.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-curative-flux.png",
     "xws": "curativeflux"
   },
   {
     "name": "cursed ground",
     "points": 19,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-cursed-ground.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-cursed-ground.png",
     "xws": "cursedground"
   },
   {
     "name": "deep contemplation",
     "points": 20,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-deep-contemplation.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-deep-contemplation.png",
     "xws": "deepcontemplation"
   },
   {
     "name": "dimensional divide",
     "points": 21,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-dimensional-divide.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-dimensional-divide.png",
     "xws": "dimensionaldivide"
   },
   {
     "name": "dimensional transfer",
     "points": 22,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-dimensional-transfer.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-dimensional-transfer.png",
     "xws": "dimensionaltransfer"
   },
   {
     "name": "duality shards",
     "points": 23,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-duality-shards.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-duality-shards.png",
     "xws": "dualityshards"
   },
   {
     "name": "enfeebling hex",
     "points": 24,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-enfeebling-hex.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-enfeebling-hex.png",
     "xws": "enfeeblinghex"
   },
   {
     "name": "envision the course",
     "points": 25,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-envision-the-course.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-envision-the-course.png",
     "xws": "envisionthecourse"
   },
   {
     "name": "ethereal vortex",
     "points": 26,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-ethereal-vortex.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-ethereal-vortex.png",
     "xws": "etherealvortex"
   },
   {
     "name": "gift of the void",
     "points": 27,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-gift-of-the-void.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-gift-of-the-void.png",
     "xws": "giftofthevoid"
   },
   {
     "name": "hand of destiny",
     "points": 28,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-hand-of-destiny.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-hand-of-destiny.png",
     "xws": "handofdestiny"
   },
   {
     "name": "inspiration from beyond",
     "points": 29,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-inspiration-from-beyond.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-inspiration-from-beyond.png",
     "xws": "inspirationfrombeyond"
   },
   {
     "name": "otherworldly journey",
     "points": 30,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-otherworldly-journey.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-otherworldly-journey.png",
     "xws": "otherworldlyjourney"
   },
   {
     "name": "peer into battle",
     "points": 31,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-peer-into-battle.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-peer-into-battle.png",
     "xws": "peerintobattle"
   },
   {
     "name": "planar fissure",
     "points": 32,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-planar-fissure.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-planar-fissure.png",
     "xws": "planarfissure"
   },
   {
     "name": "preordain the path",
     "points": 33,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-preordain-the-path.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-preordain-the-path.png",
     "xws": "preordainthepath"
   },
   {
     "name": "protective aura",
     "points": 34,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-protective-aura.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-protective-aura.png",
     "xws": "protectiveaura"
   },
   {
     "name": "ray of light",
     "points": 35,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-ray-of-light.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-ray-of-light.png",
     "xws": "rayoflight"
   },
   {
     "name": "revitalizing fount",
     "points": 36,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-revitalizing-fount.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-revitalizing-fount.png",
     "xws": "revitalizingfount"
   },
   {
     "name": "seal their fate",
     "points": 37,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-seal-their-fate.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-seal-their-fate.png",
     "xws": "sealtheirfate"
   },
   {
     "name": "void snare",
     "points": 38,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-void-snare.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-void-snare.png",
     "xws": "voidsnare"
   },
   {
     "name": "ghost falcons",
     "points": 39,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-envision-the-course.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-envision-the-course.png",
     "xws": "envisionthecourse"
   },
   {
     "name": "twilight archon",
     "points": 40,
     "expansion": "Forgotten Circles",
-    "image": "forgotten-circles/character-ability-cards-revised/DR/fc-planar-fissure.png",
+    "image": "character-ability-cards-revised/forgotten-circles/DR/fc-planar-fissure.png",
     "xws": "planarfissure"
   }
 ]
