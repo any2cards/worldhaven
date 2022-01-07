9940c30f6d96002ced48b738c8a69c5afe4ff6a1
any2cards <any2cards@yahoo.com>
2022-01-07 11:15:12 -0800

    Implement Final CS PQ

--- data/personal-quests.js
+++ data/personal-quests.js
@@ -4,342 +4,657 @@
     "points": 0,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-back.png",
-    "xws": "pqback"
+    "xws": "ghpqback"
   },  
   {
     "name": "gh-pq-510",
     "points": 1,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-510.png",
-    "xws": "pq510"
+    "xws": "ghpq510"
   },
   {
     "name": "seeker of xorn",
     "points": 2,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-510.png",
-    "xws": "pq510"
+    "xws": "ghpq510"
   },
   {
     "name": "gh-pq-511",
     "points": 3,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-511.png",
-    "xws": "pq511"
+    "xws": "ghpq511"
   },
   {
     "name": "merchant class",
     "points": 4,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-511.png",
-    "xws": "pq511"
+    "xws": "ghpq511"
   },
   {
     "name": "gh-pq-512",
     "points": 5,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-512.png",
-    "xws": "pq512"
+    "xws": "ghpq512"
   },
   {
     "name": "greed is good",
     "points": 6,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-512.png",
-    "xws": "pq512"
+    "xws": "ghpq512"
   },
   {
     "name": "gh-pq-513",
     "points": 7,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-513.png",
-    "xws": "pq513"
+    "xws": "ghpq513"
   },
   {
     "name": "finding the cure",
     "points": 8,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-513.png",
-    "xws": "pq513"
+    "xws": "ghpq513"
   },
   {
     "name": "gh-pq-514",
     "points": 9,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-514.png",
-    "xws": "pq514"
+    "xws": "ghpq514"
   },
   {
     "name": "a study of anatomy",
     "points": 10,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-514.png",
-    "xws": "pq514"
+    "xws": "ghpq514"
   },
   {
     "name": "gh-pq-515",
     "points": 11,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-515.png",
-    "xws": "pq515"
+    "xws": "ghpq515"
   },
   {
     "name": "law bringer",
     "points": 12,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-515.png",
-    "xws": "pq515"
+    "xws": "ghpq515"
   },
   {
     "name": "gh-pq-516",
     "points": 13,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-516.png",
-    "xws": "pq516"
+    "xws": "ghpq516"
   },
   {
     "name": "pounds of flesh",
     "points": 14,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-516.png",
-    "xws": "pq516"
+    "xws": "ghpq516"
   },
   {
     "name": "gh-pq-517",
     "points": 15,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-517.png",
-    "xws": "pq517"
+    "xws": "ghpq517"
   },
   {
     "name": "trophy hunt",
     "points": 16,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-517.png",
-    "xws": "pq517"
+    "xws": "ghpq517"
   },
   {
     "name": "gh-pq-518",
     "points": 17,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-518.png",
-    "xws": "pq518"
+    "xws": "ghpq518"
   },
   {
     "name": "eternal wanderer",
     "points": 18,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-518.png",
-    "xws": "pq518"
+    "xws": "ghpq518"
   },
   {
     "name": "gh-pq-519",
     "points": 19,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-519.png",
-    "xws": "pq519"
+    "xws": "ghpq519"
   },
   {
     "name": "battle legend",
     "points": 20,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-519.png",
-    "xws": "pq519"
+    "xws": "ghpq519"
   },
   {
     "name": "gh-pq-520",
     "points": 21,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-520.png",
-    "xws": "pq520"
+    "xws": "ghpq520"
   },
   {
     "name": "implement of light",
     "points": 22,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-520.png",
-    "xws": "pq520"
+    "xws": "ghpq520"
   },
   {
     "name": "gh-pq-521",
     "points": 23,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-521.png",
-    "xws": "pq521"
+    "xws": "ghpq521"
   },
   {
     "name": "take back the trees",
     "points": 24,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-521.png",
-    "xws": "pq521"
+    "xws": "ghpq521"
   },
   {
     "name": "gh-pq-522",
     "points": 25,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-522.png",
-    "xws": "pq522"
+    "xws": "ghpq522"
   },
   {
     "name": "the thin places",
     "points": 26,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-522.png",
-    "xws": "pq522"
+    "xws": "ghpq522"
   },
   {
     "name": "gh-pq-523",
     "points": 27,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-523.png",
-    "xws": "pq523"
+    "xws": "ghpq523"
   },
   {
     "name": "aberrant slayer",
     "points": 28,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-523.png",
-    "xws": "pq523"
+    "xws": "ghpq523"
   },
   {
     "name": "gh-pq-524",
     "points": 29,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-524.png",
-    "xws": "pq524"
+    "xws": "ghpq524"
   },
   {
     "name": "fearless stand",
     "points": 30,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-524.png",
-    "xws": "pq524"
+    "xws": "ghpq524"
   },
   {
     "name": "gh-pq-525",
     "points": 31,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-525.png",
-    "xws": "pq525"
+    "xws": "ghpq525"
   },
   {
     "name": "piety in all things",
     "points": 32,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-525.png",
-    "xws": "pq525"
+    "xws": "ghpq525"
   },
   {
     "name": "gh-pq-526",
     "points": 33,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-526.png",
-    "xws": "pq526"
+    "xws": "ghpq526"
   },
   {
     "name": "vengeance",
     "points": 34,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-526.png",
-    "xws": "pq526"
+    "xws": "ghpq526"
   },
   {
     "name": "gh-pq-527",
     "points": 35,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-527.png",
-    "xws": "pq527"
+    "xws": "ghpq527"
   },
   {
     "name": "zealot of the blood god",
     "points": 36,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-527.png",
-    "xws": "pq527"
+    "xws": "ghpq527"
   },
   {
     "name": "gh-pq-528",
     "points": 37,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-528.png",
-    "xws": "pq528"
+    "xws": "ghpq528"
   },
   {
     "name": "goliath toppler",
     "points": 38,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-528.png",
-    "xws": "pq528"
+    "xws": "ghpq528"
   },
   {
     "name": "gh-pq-529",
     "points": 39,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-529.png",
-    "xws": "pq529"
+    "xws": "ghpq529"
   },
   {
     "name": "the fall of man",
     "points": 40,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-529.png",
-    "xws": "pq529"
+    "xws": "ghpq529"
   },
   {
     "name": "gh-pq-530",
     "points": 41,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-530.png",
-    "xws": "pq530"
+    "xws": "ghpq530"
   },
   {
     "name": "augmented abilities",
     "points": 42,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-530.png",
-    "xws": "pq530"
+    "xws": "ghpq530"
   },
   {
     "name": "gh-pq-531",
     "points": 43,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-531.png",
-    "xws": "pq531"
+    "xws": "ghpq531"
   },
   {
     "name": "elemental samples",
     "points": 44,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-531.png",
-    "xws": "pq531"
+    "xws": "ghpq531"
   },
   {
     "name": "gh-pq-532",
     "points": 45,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-532.png",
-    "xws": "pq532"
+    "xws": "ghpq532"
   },
   {
     "name": "a helping hand",
     "points": 46,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-532.png",
-    "xws": "pq532"
+    "xws": "ghpq532"
   },
   {
     "name": "gh-pq-533",
     "points": 47,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-533.png",
-    "xws": "pq533"
+    "xws": "ghpq533"
   },
   {
     "name": "the perfect poison",
     "points": 48,
     "expansion": "Gloomhaven",
     "image": "personal-quests/gloomhaven/gh-pq-533.png",
-    "xws": "pq533"
+    "xws": "ghpq533"
+  },
+  {
+    "name": "cs-pq-back",
+    "points": 49,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-back.png",
+    "xws": "cspqback"
+  },  
+  {
+    "name": "cs-pq-330",
+    "points": 50,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-330.png",
+    "xws": "cspq330"
+  },
+  {
+    "name": "protect and server",
+    "points": 51,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-330.png",
+    "xws": "cspq330"
+  },
+  {
+    "name": "cs-pq-331",
+    "points": 52,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-331.png",
+    "xws": "cspq331"
+  },
+  {
+    "name": "weapons specialist",
+    "points": 53,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-331.png",
+    "xws": "cspq331"
+  },
+  {
+    "name": "cs-pq-332",
+    "points": 54,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-332.png",
+    "xws": "cspq332"
+  },
+  {
+    "name": "experimentation",
+    "points": 55,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-332.png",
+    "xws": "cspq332"
+  },
+  {
+    "name": "cs-pq-333",
+    "points": 56,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-333.png",
+    "xws": "cspq333"
+  },
+  {
+    "name": "thrill seeker",
+    "points": 57,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-333.png",
+    "xws": "cspq333"
+  },
+  {
+    "name": "cs-pq-334",
+    "points": 58,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-334.png",
+    "xws": "cspq334"
+  },
+  {
+    "name": "trap setter",
+    "points": 59,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-334.png",
+    "xws": "cspq334"
+  },
+  {
+    "name": "cs-pq-335",
+    "points": 60,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-335.png",
+    "xws": "cspq335"
+  },
+  {
+    "name": "bandit banisher",
+    "points": 61,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-335.png",
+    "xws": "cspq335"
+  },
+  {
+    "name": "cs-pq-336",
+    "points": 62,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-336.png",
+    "xws": "cspq336"
+  },
+  {
+    "name": "creatures in the night",
+    "points": 63,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-336.png",
+    "xws": "cspq336"
+  },
+  {
+    "name": "cs-pq-337",
+    "points": 64,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-337.png",
+    "xws": "cspq337"
+  },
+  {
+    "name": "experienced leader",
+    "points": 65,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-337.png",
+    "xws": "cspq337"
+  },
+  {
+    "name": "cs-pq-338",
+    "points": 66,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-338.png",
+    "xws": "cspq338"
+  },
+  {
+    "name": "adrenaline spike",
+    "points": 67,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-338.png",
+    "xws": "cspq338"
+  },
+  {
+    "name": "cs-pq-339",
+    "points": 68,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-339.png",
+    "xws": "cspq339"
+  },
+  {
+    "name": "mutual support",
+    "points": 69,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-339.png",
+    "xws": "cspq339"
+  },
+  {
+    "name": "cs-pq-340",
+    "points": 70,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-340.png",
+    "xws": "cspq340"
+  },
+  {
+    "name": "thy be blessed",
+    "points": 71,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-340.png",
+    "xws": "cspq340"
+  },
+  {
+    "name": "cs-pq-341",
+    "points": 72,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-341.png",
+    "xws": "cspq341"
+  },
+  {
+    "name": "spiritual gains",
+    "points": 73,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-341.png",
+    "xws": "cspq341"
+  },
+  {
+    "name": "cs-pq-342",
+    "points": 74,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-342.png",
+    "xws": "cspq342"
+  },
+  {
+    "name": "the path of agony",
+    "points": 75,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-342.png",
+    "xws": "cspq342"
+  },
+  {
+    "name": "cs-pq-343",
+    "points": 76,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-343.png",
+    "xws": "cspq343"
+  },
+  {
+    "name": "the dying of light",
+    "points": 77,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-343.png",
+    "xws": "cspq343"
+  },
+  {
+    "name": "cs-pq-344",
+    "points": 78,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-344.png",
+    "xws": "cspq344"
+  },
+  {
+    "name": "natural selection",
+    "points": 79,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-344.png",
+    "xws": "cspq344"
+  },
+  {
+    "name": "cs-pq-345",
+    "points": 80,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-345.png",
+    "xws": "cspq345"
+  },
+  {
+    "name": "predator and prey",
+    "points": 81,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-345.png",
+    "xws": "cspq345"
+  },
+  {
+    "name": "cs-pq-346",
+    "points": 82,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-346.png",
+    "xws": "cspq346"
+  },
+  {
+    "name": "add adder divides",
+    "points": 83,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-346.png",
+    "xws": "cspq346"
+  },
+  {
+    "name": "cs-pq-347",
+    "points": 84,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-347.png",
+    "xws": "cspq347"
+  },
+  {
+    "name": "field research",
+    "points": 85,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-347.png",
+    "xws": "cspq347"
+  },
+  {
+    "name": "cs-pq-348",
+    "points": 86,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-348.png",
+    "xws": "cspq348"
+  },
+  {
+    "name": "conjurers hand",
+    "points": 87,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-348.png",
+    "xws": "cspq348"
+  },
+  {
+    "name": "cs-pq-349",
+    "points": 88,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-349.png",
+    "xws": "cspq349"
+  },
+  {
+    "name": "no rest for the wicked",
+    "points": 89,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-349.png",
+    "xws": "cspq349"
+  },
+  {
+    "name": "cs-pq-350",
+    "points": 90,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-350.png",
+    "xws": "cspq350"
+  },
+  {
+    "name": "health first",
+    "points": 91,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-350.png",
+    "xws": "cspq350"
+  },
+  {
+    "name": "cs-pq-351",
+    "points": 92,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-351.png",
+    "xws": "cspq351"
+  },
+  {
+    "name": "limitless searching",
+    "points": 93,
+    "expansion": "Crimson Scales",
+    "image": "personal-quests/crimson-scales/cs-pq-351.png",
+    "xws": "cspq351"
   }
 ]
