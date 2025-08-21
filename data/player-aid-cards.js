6445fe8c0022b3554b86b724de9cf744e1db77f5
any2cards <any2cards@yahoo.com>
2025-08-20 19:25:25 -0700

    Implement New GH PAC, RD, & RS Assets with Copyright, Standardized Sizes & Asset Numbers

--- data/player-aid-cards.js
+++ data/player-aid-cards.js
@@ -22,5 +22,47 @@
     "image": "player-aid-cards/gloomhaven/gh-player-reference-back.png",
     "xws": "playerreference",
     "assetno": "###"
+  },
+  {
+    "name": "halt bottom front",
+    "points": 3,
+    "expansion": "forgotten circles",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-bottom-front.png",
+    "xws": "haltbottomfront"
+  },
+  {
+    "name": "halt bottom back",
+    "points": 4,
+    "expansion": "forgotten circles",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-bottom-back.png",
+    "xws": "haltbottomback"
+  },
+  {
+    "name": "halt top front",
+    "points": 5,
+    "expansion": "forgotten circles",                                                                                           {
+    "image": "player-aid-cards/forgotten-circles/fc-halt-top-front.png",
+    "xws": "halttopfront"
+  },
+  {
+    "name": "halt top back",
+    "points": 6,
+    "expansion": "forgotten circles",
+    "image": "player-aid-cards/forgotten-circles/fc-halt-top-back.png",
+    "xws": "halttopback"
+  },
+  {
+    "name": "player reference front",
+    "points": 7,
+    "expansion": "forgotten circles",
+    "image": "player-aid-cards/forgotten-circles/fc-player-reference-front.png",
+    "xws": "playerreferencefront"
+  },
+  {
+    "name": "player reference back",
+    "points": 8,
+    "expansion": "forgotten circles",
+    "image": "player-aid-cards/forgotten-circles/fc-player-reference-back.png",
+    "xws": "playerreferenceback"
   }
 ]
