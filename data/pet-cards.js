4098883bd22cdac7eed20aef6520a9cd9bc169c9
any2cards <any2cards@yahoo.com>
2025-10-29 10:33:43 -0700

    Implement New FH PC Assets with Copyright, Standardized Sizes & Asset Numbers

--- data/pet-cards.js
+++ data/pet-cards.js
@@ -4,167 +4,383 @@
     "points": 0,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-01-piranha-pig.png",
-    "xws": "01piranhapig"
+    "xws": "01piranhapig",
+    "assetno": "0361"
+  },
+  {
+    "name": "0361",
+    "points": 1,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-01-piranha-pig.png",
+    "xws": "01piranhapig",
+    "assetno": "0361"
   },
   {
     "name": "01 piranha pig",
-    "points": 1,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-01-piranha-pig-back.png",
-    "xws": "01piranhapig"
-  },
-  {
-    "name": "02 hound",
     "points": 2,
     "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-02-hound.png",
-    "xws": "02hound"
+    "image": "pet-cards/frosthaven/fh-01-piranha-pig-back.png",
+    "xws": "01piranhapig",
+    "assetno": "0361"
+  },
+  {
+    "name": "0361",
+    "points": 3,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-01-piranha-pig-back.png",
+    "xws": "01piranhapig",
+    "assetno": "0361"
   },
   {
     "name": "02 hound",
-    "points": 3,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-02-hound-back.png",
-    "xws": "02hound"
-  },
-  {
-    "name": "03 spitting drake",
     "points": 4,
     "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-03-spitting-drake.png",
-    "xws": "03spittingdrake"
+    "image": "pet-cards/frosthaven/fh-02-hound.png",
+    "xws": "02hound",
+    "assetno": "0362"
+  },
+  {
+    "name": "0362",
+    "points": 5,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-02-hound.png",
+    "xws": "02hound",
+    "assetno": "0362"
+  },
+  {
+    "name": "02 hound",
+    "points": 6,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-02-hound-back.png",
+    "xws": "02hound",
+    "assetno": "0362"
+  },
+  {
+    "name": "0362",
+    "points": 7,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-02-hound-back.png",
+    "xws": "02hound",
+    "assetno": "0362"
   },
   {
     "name": "03 spitting drake",
-    "points": 5,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-03-spitting-drake-back.png",
-    "xws": "03spittingdrake"
-  },
-  {
-    "name": "04 rending drake",
-    "points": 6,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-04-rending-drake.png",
-    "xws": "04rendingdrake"
-  },
-  {
-    "name": "04 rending drake",
-    "points": 7,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-04-rending-drake-back.png",
-    "xws": "04rendingdrake"
-  },
-  {
-    "name": "05 black imp",
     "points": 8,
     "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-05-black-imp.png",
-    "xws": "05blackimp"
+    "image": "pet-cards/frosthaven/fh-03-spitting-drake.png",
+    "xws": "03spittingdrake",
+    "assetno": "0363"
+  },
+  {
+    "name": "0363",
+    "points": 9,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-03-spitting-drake.png",
+    "xws": "03spittingdrake",
+    "assetno": "0363"
+  },
+  {
+    "name": "03 spitting drake",
+    "points": 10,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-03-spitting-drake-back.png",
+    "xws": "03spittingdrake",
+    "assetno": "0363"
+  },
+  {
+    "name": "0363",
+    "points": 11,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-03-spitting-drake-back.png",
+    "xws": "03spittingdrake",
+    "assetno": "0363"
+  },
+  {
+    "name": "04 rending drake",
+    "points": 12,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-04-rending-drake.png",
+    "xws": "04rendingdrake",
+    "assetno": "0364"
+  },
+  {
+    "name": "0364",
+    "points": 13,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-04-rending-drake.png",
+    "xws": "04rendingdrake",
+    "assetno": "0364"
+  },
+  {
+    "name": "04 rending drake",
+    "points": 14,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-04-rending-drake-back.png",
+    "xws": "04rendingdrake",
+    "assetno": "0364"
+  },
+  {
+    "name": "0364",
+    "points": 15,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-04-rending-drake-back.png",
+    "xws": "04rendingdrake",
+    "assetno": "0364"
   },
   {
     "name": "05 black imp",
-    "points": 9,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-05-black-imp-back.png",
-    "xws": "05blackimp"
-  },
-  {
-    "name": "06 forest imp",
-    "points": 10,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-06-forest-imp.png",
-    "xws": "06forestimp"
-  },
-  {
-    "name": "06 forest imp",
-    "points": 11,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-06-forest-imp-back.png",
-    "xws": "06forestimp"
-  },
-  {
-    "name": "07 snow imp",
-    "points": 12,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-07-snow-imp.png",
-    "xws": "07snowimp"
-  },
-  {
-    "name": "07 snow imp",
-    "points": 13,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-07-snow-imp-back.png",
-    "xws": "07snowimp"
-  },
-  {
-    "name": "08 ooze",
-    "points": 14,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-08-ooze.png",
-    "xws": "08ooze"
-  },
-  {
-    "name": "08 ooze",
-    "points": 15,
-    "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-08-ooze-back.png",
-    "xws": "08ooze"
-  },
-  {
-    "name": "09 ruined machine",
     "points": 16,
     "expansion": "frosthaven",
-    "image": "pet-cards/frosthaven/fh-09-ruined-machine.png",
-    "xws": "09ruinedmachine"
+    "image": "pet-cards/frosthaven/fh-05-black-imp.png",
+    "xws": "05blackimp",
+    "assetno": "0365"
+  },
+  {
+    "name": "0365",
+    "points": 17,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-05-black-imp.png",
+    "xws": "05blackimp",
+    "assetno": "0365"
+  },
+  {
+    "name": "05 black imp",
+    "points": 18,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-05-black-imp-back.png",
+    "xws": "05blackimp",
+    "assetno": "0365"
+  },
+  {
+    "name": "0365",
+    "points": 19,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-05-black-imp-back.png",
+    "xws": "05blackimp",
+    "assetno": "0365"
+  },
+  {
+    "name": "06 forest imp",
+    "points": 20,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-06-forest-imp.png",
+    "xws": "06forestimp",
+    "assetno": "0366"
+  },
+  {
+    "name": "0366",
+    "points": 21,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-06-forest-imp.png",
+    "xws": "06forestimp",
+    "assetno": "0366"
+  },
+  {
+    "name": "06 forest imp",
+    "points": 22,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-06-forest-imp-back.png",
+    "xws": "06forestimp",
+    "assetno": "0366"
+  },
+  {
+    "name": "0366",
+    "points": 23,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-06-forest-imp-back.png",
+    "xws": "06forestimp",
+    "assetno": "0366"
+  },
+  {
+    "name": "07 snow imp",
+    "points": 24,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-07-snow-imp.png",
+    "xws": "07snowimp",
+    "assetno": "0367"
+  },
+  {
+    "name": "0367",
+    "points": 25,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-07-snow-imp.png",
+    "xws": "07snowimp",
+    "assetno": "0367"
+  },
+  {
+    "name": "07 snow imp",
+    "points": 26,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-07-snow-imp-back.png",
+    "xws": "07snowimp",
+    "assetno": "0367"
+  },
+  {
+    "name": "0367",
+    "points": 27,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-07-snow-imp-back.png",
+    "xws": "07snowimp",
+    "assetno": "0367"
+  },
+  {
+    "name": "08 ooze",
+    "points": 28,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-08-ooze.png",
+    "xws": "08ooze",
+    "assetno": "0368"
+  },
+  {
+    "name": "0368",
+    "points": 29,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-08-ooze.png",
+    "xws": "08ooze",
+    "assetno": "0368"
+  },
+  {
+    "name": "08 ooze",
+    "points": 30,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-08-ooze-back.png",
+    "xws": "08ooze",
+    "assetno": "0368"
+  },
+  {
+    "name": "0368",
+    "points": 31,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-08-ooze-back.png",
+    "xws": "08ooze",
+    "assetno": "0368"
   },
   {
     "name": "09 ruined machine",
-    "points": 17,
+    "points": 32,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-09-ruined-machine.png",
+    "xws": "09ruinedmachine",
+    "assetno": "0369"
+  },
+  {
+    "name": "0369",
+    "points": 33,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-09-ruined-machine.png",
+    "xws": "09ruinedmachine",
+    "assetno": "0369"
+  },
+  {
+    "name": "09 ruined machine",
+    "points": 34,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-09-ruined-machine-back.png",
-    "xws": "09ruinedmachine"
+    "xws": "09ruinedmachine",
+    "assetno": "0369"
+  },
+  {
+    "name": "0369",
+    "points": 35,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-09-ruined-machine-back.png",
+    "xws": "09ruinedmachine",
+    "assetno": "0369"
   },
   {
     "name": "10 lightning eel",
-    "points": 18,
+    "points": 36,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-10-lightning-eel.png",
-    "xws": "10lightningeel"
+    "xws": "10lightningeel",
+    "assetno": "0370"
+  },
+  {
+    "name": "0370",
+    "points": 37,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-10-lightning-eel.png",
+    "xws": "10lightningeel",
+    "assetno": "0370"
   },
   {
     "name": "10 lightning eel",
-    "points": 19,
+    "points": 38,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-10-lightning-eel-back.png",
-    "xws": "10lightningeel"
+    "xws": "10lightningeel",
+    "assetno": "0370"
+  },
+  {
+    "name": "0370",
+    "points": 39,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-10-lightning-eel-back.png",
+    "xws": "10lightningeel",
+    "assetno": "0370"
   },
   {
     "name": "11 heroics",
-    "points": 20,
+    "points": 40,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-11-heroics.png",
-    "xws": "11heroics"
+    "xws": "11heroics",
+    "assetno": "0371"
+  },
+  {
+    "name": "0371",
+    "points": 41,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-11-heroics.png",
+    "xws": "11heroics",
+    "assetno": "0371"
   },
   {
     "name": "11 heroics",
-    "points": 21,
+    "points": 42,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-11-heroics-back.png",
-    "xws": "11heroics"
+    "xws": "11heroics",
+    "assetno": "0371"
+  },
+  {
+    "name": "0371",
+    "points": 43,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-11-heroics-back.png",
+    "xws": "11heroics",
+    "assetno": "0371"
   },
   {
     "name": "12 brummix",
-    "points": 22,
+    "points": 44,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-12-brummix.png",
-    "xws": "12brummix"
+    "xws": "12brummix",
+    "assetno": "0372"
+  },
+  {
+    "name": "0372",
+    "points": 45,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-12-brummix.png",
+    "xws": "12brummix",
+    "assetno": "0372"
   },
   {
     "name": "12 brummix",
-    "points": 23,
+    "points": 46,
     "expansion": "frosthaven",
     "image": "pet-cards/frosthaven/fh-12-brummix-back.png",
-    "xws": "12brummix"
+    "xws": "12brummix",
+    "assetno": "0372"
+  },
+  {
+    "name": "0372",
+    "points": 47,
+    "expansion": "frosthaven",
+    "image": "pet-cards/frosthaven/fh-12-brummix-back.png",
+    "xws": "12brummix",
+    "assetno": "0372"
   }
 ]
