11f58bd082710a40764a25f8d083031211ecc090
any2cards <any2cards@yahoo.com>
2025-10-30 17:58:44 -0700

    Implement New FH RS Assets with Copyright, Standardized Sizes & Asset Numbers

--- data/random-scenarios.js
+++ data/random-scenarios.js
@@ -224,134 +224,198 @@
     "assetno": "490"
   },
   {
-    "name": "random scenario back",
+    "name": "random scenario 26.4",
     "points": 28,
     "expansion": "frosthaven",
-    "image": "random-scenarios/frosthaven/fh-random-scenario-back.png",
-    "xws": "randomscenarioback"
+    "image": "random-scenarios/frosthaven/fh-random-scenario-26.4.png",
+    "xws": "randomscenario264",
+    "assetno": "1475"
   },
   {
-    "name": "random scenario 26.4",
+    "name": "1475",
     "points": 29,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-26.4.png",
-    "xws": "randomscenario264"
+    "xws": "randomscenario264",
+    "assetno": "1475"
   },
   {
     "name": "random scenario 81.1",
     "points": 30,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-81.1.png",
-    "xws": "randomscenario811"
+    "xws": "randomscenario811",
+    "assetno": "1477"
+  },
+  {
+    "name": "1477",
+    "points": 31,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-81.1.png",
+    "xws": "randomscenario811",
+    "assetno": "1477"
   },
   {
     "name": "random scenario 119.3",
-    "points": 31,
+    "points": 32,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-119.3.png",
-    "xws": "randomscenario1193"
+    "xws": "randomscenario1193",
+    "assetno": "1478"
+  },
+  {
+    "name": "1478",
+    "points": 33,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-119.3.png",
+    "xws": "randomscenario1193",
+    "assetno": "1478"
   },
   {
     "name": "random scenario 142.4",
-    "points": 32,
+    "points": 34,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-142.4.png",
-    "xws": "randomscenario1424"
+    "xws": "randomscenario1424",
+    "assetno": "1481"
+  },
+  {
+    "name": "1481",
+    "points": 35,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-142.4.png",
+    "xws": "randomscenario1424",
+    "assetno": "1481"
   },
   {
     "name": "random scenario 146.4",
-    "points": 33,
+    "points": 36,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-146.4.png",
-    "xws": "randomscenario1464"
+    "xws": "randomscenario1464",
+    "assetno": "1479"
+  },
+  {
+    "name": "1479",
+    "points": 37,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-146.4.png",
+    "xws": "randomscenario1464",
+    "assetno": "1479"
   },
   {
     "name": "random scenario 171.2",
-    "points": 34,
+    "points": 38,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-171.2.png",
-    "xws": "randomscenario1712"
+    "xws": "randomscenario1712",
+    "assetno": "1476"
+  },
+  {
+    "name": "1476",
+    "points": 39,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-171.2.png",
+    "xws": "randomscenario1712",
+    "assetno": "1476"
   },
   {
     "name": "random scenario 186.5",
-    "points": 35,
+    "points": 40,
     "expansion": "frosthaven",
     "image": "random-scenarios/frosthaven/fh-random-scenario-186.5.png",
-    "xws": "randomscenario1865"
+    "xws": "randomscenario1865",
+    "assetno": "1480"
+  },
+  {
+    "name": "1480",
+    "points": 41,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-186.5.png",
+    "xws": "randomscenario1865",
+    "assetno": "1480"
   },
   {
     "name": "random scenario back",
-    "points": 36,
+    "points": 42,
+    "expansion": "frosthaven",
+    "image": "random-scenarios/frosthaven/fh-random-scenario-back.png",
+    "xws": "randomscenarioback",
+    "assetno": "####"
+  },
+  {
+    "name": "random scenario back",
+    "points": 43,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-random-scenario-back.png",
     "xws": "toarandomscenarioback"
   },
   {
     "name": "random scenario 72",
-    "points": 37,
+    "points": 44,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-inox-salvation.png",
     "xws": "inoxsalvation"
   },
   {
     "name": "inox salvation",
-    "points": 38,
+    "points": 45,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-inox-salvation.png",
     "xws": "inoxsalvation"
   },
   {
     "name": "random scenario 73",
-    "points": 39,
+    "points": 46,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-lootpalooza.png",
     "xws": "lootpalooza"
   },
   {
     "name": "lootpalooza",
-    "points": 40,
+    "points": 47,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-lootpalooza.png",
     "xws": "lootpalooza"
   },
   {
     "name": "random scenario 74",
-    "points": 41,
+    "points": 48,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-song-of-the-harrower.png",
     "xws": "songoftheharrower"
   },
   {
     "name": "song of the harrower",
-    "points": 42,
+    "points": 49,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-song-of-the-harrower.png",
     "xws": "songoftheharrower"
   },
   {
     "name": "random scenario 75",
-    "points": 43,
+    "points": 50,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-sunken-temple.png",
     "xws": "sunkentemple"
   },
   {
     "name": "sunken temple",
-    "points": 44,
+    "points": 51,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-sunken-temple.png",
     "xws": "sunkentemple"
   },
   {
     "name": "random scenario 76",
-    "points": 45,
+    "points": 52,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-untold-woe.png",
     "xws": "untoldwoe"
   },
   {
     "name": "untold woe",
-    "points": 46,
+    "points": 53,
     "expansion": "trail of ashes",
     "image": "random-scenarios/trail-of-ashes/toa-untold-woe.png",
     "xws": "untoldwoe"
