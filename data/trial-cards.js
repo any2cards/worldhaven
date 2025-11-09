6e8364cb20cf88363ce975f0f8ce7aa58559c513
any2cards <any2cards@yahoo.com>
2025-11-09 08:38:40 -0800

    Implement New FH PC Assets with Copyright, Standardized Sizes & Asset Numbers

--- data/trial-cards.js
+++ data/trial-cards.js
@@ -4,97 +4,215 @@
     "points": 0,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-cards-back.png",
-    "xws": "trialcardsback"
+    "xws": "trialcardsback",
+    "assetno": "####"
   },
   {
     "name": "trial of censorship",
     "points": 1,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-censorship.png",
-    "xws": "trialofcensorship"
+    "xws": "trialofcensorship",
+    "assetno": "0356"
+  },
+  {
+    "name": "0356",
+    "points": 2,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-censorship.png",
+    "xws": "trialofcensorship",
+    "assetno": "0356"
   },
   {
     "name": "trial of deceit",
-    "points": 2,
+    "points": 3,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-deceit.png",
-    "xws": "trialofdeceit"
+    "xws": "trialofdeceit",
+    "assetno": "0351"
+  },
+  {
+    "name": "0351",
+    "points": 4,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-deceit.png",
+    "xws": "trialofdeceit",
+    "assetno": "0351"
   },
   {
     "name": "trial of expression",
-    "points": 3,
+    "points": 5,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-expression.png",
-    "xws": "trialofexpression"
+    "xws": "trialofexpression",
+    "assetno": "0350"
+  },
+  {
+    "name": "0350",
+    "points": 6,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-expression.png",
+    "xws": "trialofexpression",
+    "assetno": "0350"
   },
   {
     "name": "trial of inquisition",
-    "points": 4,
+    "points": 7,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-inquisition.png",
-    "xws": "trialofinquisition"
+    "xws": "trialofinquisition",
+    "assetno": "0353"
+  },
+  {
+    "name": "0353",
+    "points": 8,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-inquisition.png",
+    "xws": "trialofinquisition",
+    "assetno": "0353"
   },
   {
     "name": "trial of manipulation",
-    "points": 5,
+    "points": 9,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-manipulation.png",
-    "xws": "trialofmanipulation"
+    "xws": "trialofmanipulation",
+    "assetno": "0358"
+  },
+  {
+    "name": "0358",
+    "points": 10,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-manipulation.png",
+    "xws": "trialofmanipulation",
+    "assetno": "0358"
   },
   {
     "name": "trial of order",
-    "points": 6,
+    "points": 11,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-order.png",
-    "xws": "trialoforder"
+    "xws": "trialoforder",
+    "assetno": "0355"
+  },
+  {
+    "name": "0355",
+    "points": 12,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-order.png",
+    "xws": "trialoforder",
+    "assetno": "0355"
   },
   {
     "name": "trial of patience",
-    "points": 7,
+    "points": 13,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-patience.png",
-    "xws": "trialofpatience"
+    "xws": "trialofpatience",
+    "assetno": "0349"
+  },
+  {
+    "name": "0349",
+    "points": 14,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-patience.png",
+    "xws": "trialofpatience",
+    "assetno": "0349"
   },
   {
     "name": "trial of poverty",
-    "points": 8,
+    "points": 15,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-poverty.png",
-    "xws": "trialofpoverty"
+    "xws": "trialofpoverty",
+    "assetno": "0357"
+  },
+  {
+    "name": "0357",
+    "points": 16,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-poverty.png",
+    "xws": "trialofpoverty",
+    "assetno": "0357"
   },
   {
     "name": "trial of prediction",
-    "points": 9,
+    "points": 17,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-prediction.png",
-    "xws": "trialofprediction"
+    "xws": "trialofprediction",
+    "assetno": "0354"
+  },
+  {
+    "name": "0354",
+    "points": 18,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-prediction.png",
+    "xws": "trialofprediction",
+    "assetno": "0354"
   },
   {
     "name": "trial of strategy",
-    "points": 10,
+    "points": 19,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-strategy.png",
-    "xws": "trialofstrategy"
+    "xws": "trialofstrategy",
+    "assetno": "0359"
+  },
+  {
+    "name": "0359",
+    "points": 20,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-strategy.png",
+    "xws": "trialofstrategy",
+    "assetno": "0359"
   },
   {
     "name": "trial of trials",
-    "points": 11,
+    "points": 21,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-trials.png",
-    "xws": "trialoftrials"
+    "xws": "trialoftrials",
+    "assetno": "0352"
+  },
+  {
+    "name": "0352",
+    "points": 22,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-trials.png",
+    "xws": "trialoftrials",
+    "assetno": "0352"
   },
   {
     "name": "trial of victory",
-    "points": 12,
+    "points": 23,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-victory.png",
-    "xws": "trialofvictory"
+    "xws": "trialofvictory",
+    "assetno": "0348"
+  },
+  {
+    "name": "0348",
+    "points": 24,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-victory.png",
+    "xws": "trialofvictory",
+    "assetno": "0348"
   },
   {
     "name": "trial of weakness",
-    "points": 13,
+    "points": 25,
     "expansion": "frosthaven",
     "image": "trial-cards/frosthaven/fh-trial-of-weakness.png",
-    "xws": "trialofweakness"
+    "xws": "trialofweakness",
+    "assetno": "0360"
+  },
+  {
+    "name": "0360",
+    "points": 26,
+    "expansion": "frosthaven",
+    "image": "trial-cards/frosthaven/fh-trial-of-weakness.png",
+    "xws": "trialofweakness",
+    "assetno": "0360"
   }
 ]
